0689fb506c37fe0cd55c783e769f9dca5ec5bcd3
MrTizu <204244194+bw3h@users.noreply.github.com>
2025-04-03 19:59:47 +0300

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const Layout = resolve => require(['@/components/layouts/Layout'], resolve);
+const neatHome = resolve => require(['@/components/pages/neatHome'], resolve)
+const neatStake = resolve => require(['@/components/pages/neatStake'], resolve)
+const dashboard = resolve => require(['@/components/pages/dashboard'], resolve)
+const terms = resolve => require(['@/components/pages/terms'], resolve)
+const create = resolve => require(['@/components/pages/create'], resolve)
+const createpk = resolve => require(['@/components/pages/createPK'], resolve)
+const createks = resolve => require(['@/components/pages/createKS'], resolve)
+
+Vue.use(Router)
+const router = new Router({
+    mode: 'history',
+    routes: [{
+        path: '/',
+
+        component: Layout,
+        children: [{
+            path: '',
+            name: 'neatHome',
+            component: neatHome,
+            meta: []
+        },
+        {
+            path: '/dashboard',
+            name: 'dashboard',
+            component: dashboard,
+        },
+        {
+            path: '/create',
+            name: 'create',
+            component: create
+        },
+        {
+            path: '/createpk',
+            name: 'createpk',
+            component: createpk
+        },
+        {
+            path: '/createks',
+            name: 'createks',
+            component: createks,
+        },
+        {
+            path: '/terms',
+            name: 'terms',
+            component: terms,
+        },
+        ]
+    }]
+})
+
+export default router
